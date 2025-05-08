255b3aad274de6efaef3b901bf8fd528feb1520a
Juanita Palacios <est.juanita.palaci@unimilitar.edu.co>
2025-05-08 17:41:48 -0500

    home

---
+++ javascript/home.js
@@ -0,0 +1,37 @@
+const carousel = document.getElementById('carousel');
+const cards = document.querySelectorAll('.card');
+const arrowLeft = document.querySelector('.arrow.left');
+const arrowRight = document.querySelector('.arrow.right');
+
+let index = 0;
+
+function updateCarousel() {
+  carousel.style.transform = `translateX(-${index * 100}%)`;
+}
+
+arrowLeft.addEventListener('click', () => {
+  index = index > 0 ? index - 1 : cards.length - 1;
+  updateCarousel();
+});
+
+arrowRight.addEventListener('click', () => {
+  index = (index + 1) % cards.length;
+  updateCarousel();
+});
+
+// Swipe para móviles
+let startX = 0;
+
+carousel.addEventListener('touchstart', e => {
+  startX = e.touches[0].clientX;
+});
+
+carousel.addEventListener('touchend', e => {
+  let endX = e.changedTouches[0].clientX;
+  if (startX - endX > 50) {
+    index = (index + 1) % cards.length;
+  } else if (endX - startX > 50) {
+    index = index > 0 ? index - 1 : cards.length - 1;
+  }
+  updateCarousel();
+});
